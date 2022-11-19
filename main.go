98131f30c2c99ef1ed0e4606da0d2b52fa7b1346
fran <fran@penguin>
2022-11-19 20:38:39 -0300

    Sorteio implementado

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"encoding/csv"
 	"os"
 	"log"
+	"io"
 )
 
 type Pessoa struct {
@@ -18,17 +19,61 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-func main() {
-	fmt.Println("Amigo Secreto");
+func readPessoas() []Pessoa {
+	// Lista de pessoas
+	pessoas := []Pessoa{}
 
 	// Ler o arquivo
-	file := "Teste.csv"
-
-	content, err := os.ReadFile(file)
-
+	f, err := os.Open("Lista.csv")
+    
 	if err != nil {
-		log.Fatal(err)
+        log.Fatal(err)
+    }
+
+    // Fecha o arquivo no fim da função
+    defer f.Close()
+	
+	// Lê o CSV
+	reader := csv.NewReader(f)
+	for {
+		linha, err := reader.Read()
+
+		// Verifica se o arquivo acabou
+		if err == io.EOF {
+			break
+		}
+
+		// Verfica se deu erro
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Adiciona linha à lista
+		pessoas = append(pessoas, Pessoa{linha[0], linha[1]})
 	}
 
-	fmt.Println(string(content))
+	return pessoas
+}
+
+// From:
+	// https://stackoverflow.com/questions/12264789/shuffle-array-in-go - go implementation
+	// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle - algorithm
+
+func shuffleList(lista []Pessoa) []Pessoa {
+	for i := range lista {
+		j := rand.Intn(i + 1)
+		lista[i], lista[j] = lista[j], lista[i]
+	}
+
+	return lista
+}
+
+func main() {
+	fmt.Println("++ Amigo Secreto");
+	lista := shuffleList(readPessoas())
+
+	for i := range lista {
+		fmt.Println(lista[i].Nome)
+		fmt.Println(lista[i+1].Nome)
+	}
 }
