18f74a11efcdd02f530697b3c3abde2e7dfb572b
franpessoa <frandevacc@gmail.com>
2022-11-26 12:02:01 -0300

    Adiciona um \n no fim da string

--- debug.go
+++ debug.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func Debug(pessoa Pessoa) {
-	fmt.Printf("%v tirou %v", pessoa.Nome, pessoa.AmigoSecreto)
+	fmt.Printf("%v tirou %v\n", pessoa.Nome, pessoa.AmigoSecreto)
 }
