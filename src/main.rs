7d9f6632ebd9438dcf8e39498851eedb62e31abe
Francisco Pessoa <franciscomspessoa@gmail.com>
2023-10-14 15:18:36 -0300

    Implement shuffling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::Path;
+use clap::Parser;
+use amigo_secreto::participants::{read_participants};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about)]
+struct Args {
+    #[arg(short, long)]
+    data: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let path =  Path::new(&args.data);
+    
+    let participants = read_participants(path)
+        .unwrap();
+
+    println!("{:#?}", participants);
 }
