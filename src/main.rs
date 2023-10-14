039a4f89a59040ce219b417ec08d86ad03e7be77
Francisco Pessoa <franciscomspessoa@gmail.com>
2023-10-14 19:10:00 -0300

    Create frontend

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use axum::{Router, routing::{get, post}};
 use amigo_secreto::handlers::*;
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
     let app = Router::new()
-        .route("/", get(root))
-        .route("/game", get(game));
-
+        .route("/game", get(game))
+        .nest_service("/public", ServeDir::new("public"))
+        .nest_service("/", ServeFile::new("index.html"));
+    
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
